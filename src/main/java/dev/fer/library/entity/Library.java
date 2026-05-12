5fdb7e83557e875d351c13da87d1ec1c90666a39
Jose Fernando <jose.gomezm12@gmail.com>
2026-05-12 13:29:05 -0600

    chore: add flyway and database schema

--- src/main/java/dev/fer/library/entity/Library.java
+++ src/main/java/dev/fer/library/entity/Library.java
@@ -1,22 +1,27 @@
 package dev.fer.library.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
+@Table(name = "libraries")
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
 public class Library {
 
   @Id
-  @GeneratedValue(strategy = GenerationType.AUTO)
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "id")
   private Long id;
 
+  @Column(name = "name")
   private String name;
 }
---
+++ src/main/resources/db/migration/V1__create_library_table.sql
@@ -0,0 +1,4 @@
+CREATE TABLE libraries (
+  id BIGSERIAL PRIMARY KEY,
+  name VARCHAR(255)
+)
