59f15af41ec90c81911353d86eb9d2826d2546c4
Jose Fernando <jose.gomezm12@gmail.com>
2026-05-13 14:14:18 -0600

    test: adding floor repository integration test

---
+++ src/test/java/dev/fer/library/repository/FloorRepositoryTest.java
@@ -0,0 +1,78 @@
+package dev.fer.library.repository;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
+
+import dev.fer.library.entity.Floor;
+import dev.fer.library.entity.Library;
+
+@DataJpaTest
+public class FloorRepositoryTest {
+  @Autowired
+  private FloorRepository floorRepository;
+
+  @Autowired LibraryRepository libraryRepository;
+
+  private Long floorID;
+
+  @BeforeEach
+  void setUp() {
+    Library library = libraryRepository.save(new Library(null, "Library"));
+    Floor floor = new Floor(floorID, library, "1", "null");
+    floorRepository.save(floor);
+
+    floorID = floor.getId();
+  }
+
+  @Test
+  void shouldInsertFloor() {
+    Floor floor = new Floor(floorID, new Library(null, "Library"), "1", "null");
+    
+    floorRepository.save(floor);
+    assertThat(floor.getId()).isNotNull();
+    assertThat(floor.getCode()).isEqualTo("1");
+    assertThat(floor.getLibrary()).isNotNull();
+  }
+
+  @Test
+  void shouldReturnFloorById() {
+    Floor floor = floorRepository.findById(floorID).get();
+
+    assertThat(floor.getId()).isEqualTo(floorID);
+  }
+
+  @Test
+  void shouldReturnNullWhenNotExist() {
+    Optional<Floor> floor = floorRepository.findById(999L);
+    
+    assertThat(floor.isEmpty()).isTrue();
+  }
+
+  @Test
+  void shouldReturnTrueWhenFloorExist() {
+    assertThat(floorRepository.existsById(floorID)).isTrue();
+  }
+
+  @Test
+  void shouldReturnFalseWhenFloorNotExist() {
+    assertThat(floorRepository.existsById(99L)).isFalse();
+  }
+
+  @Test
+  void shouldUpdateFloor() {
+    Floor floor = floorRepository.findById(floorID).get();
+    
+    Floor updated = new Floor(floor.getId(), floor.getLibrary(), floor.getCode(), "The new desc");
+    
+    floorRepository.save(updated);
+
+    assertThat(floor.getDescription()).isEqualTo(updated.getDescription());
+    assertThat(floor.getId()).isEqualTo(updated.getId());
+  }
+}
